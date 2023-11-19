148dd04d810f4765fe223d315eb58145805bb7c4
Dediaev Danil <dediaev.danil130702@gmail.com>
2023-11-19 18:48:33 +0300

    create clientGenerator

--- src/main/kotlin/ClientGenerator.kt
+++ src/main/kotlin/ClientGenerator.kt
@@ -4,21 +4,24 @@ import java.util.Random
 
 class ClientGenerator {
     fun generateClientList(size: Int): List<Client> {
-        TODO()
-
+        val clientList: MutableList<Client> = mutableListOf()
+        for (i in 1..size) {
+            clientList.add(createRandomClient(i))
+        }
+        return clientList
     }
 
-    private fun createOrder(size: Int): List<Number> {
-        val randomizer = Random()
-        val dishesNum = randomizer.nextInt(1, 7)
-        TODO()
-
+    private fun createOrder(size: Int): List<Dish> {
+        val shuffledDishes = Dish.entries.toList().shuffled()
+        val newOrder = shuffledDishes.take(size)
+        return newOrder
     }
 
     private fun createRandomClient(id: Int): Client {
         val randomizer = Random()
         val orderTime = randomizer.nextInt(1, 6)
-//        val order = Order
-        TODO()
+        val dishesNum = randomizer.nextInt(1, 7)
+        val order = createOrder(dishesNum)
+        return Client(orderTime, order, id)
     }
 }
