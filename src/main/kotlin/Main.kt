027d25b60f8934a2a602103591d3f55d20c7c7a8
Dediaev Danil <dediaev.danil130702@gmail.com>
2023-11-24 02:42:37 +0300

    add new files

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,34 +2,27 @@ import org.example.ClientGenerator
 import kotlin.system.measureTimeMillis
 
 suspend fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
     val clientGenerator = ClientGenerator()
-    val clients = clientGenerator.generateClientList(1000)
-    val terminalController1 = TerminalController(name="one-thread",1,clients.size)
+    val clients = clientGenerator.generateClientList(10000)
 
 
-//    val terminal = Terminal()
-//    val executionTime1 = measureTimeMillis {
-//        for (client in clients) {
-//            terminal.serveClient(client)
-//        }
-//    }
+    val terminalController1 = TerminalController(name="ten-thread",10,clients.size)
+
     for (i in clients.indices) {
         terminalController1.serveClient(clients[i], i)
-
     }
 
-    val terminalController2 = TerminalController(name="two-thread",1,clients.size)
+    val terminalController2 = TerminalController(name="hundred-thread",100, clients.size)
     for (i in clients.indices) {
         terminalController2.serveClient(clients[i], i)
     }
 
-
-////    println("Время работы кода 1: ${(executionTime1 / 1000.0)} секунд")
-//    println("Время работы кода 2: ${(executionTime2 / 1000.0)} секунд")
-//    println("Время работы кода 3: ${(executionTime3 / 1000.0)} секунд")
+//    val terminal = Terminal()
+//    val executionTime = measureTimeMillis {
+//        for (client in clients) {
+//            terminal.serveClient(client)
+//        }
+//    }
+//    println("Время выполнения: $timeInSeconds милисекунд")
 }
--- src/main/kotlin/TerminalController.kt
+++ src/main/kotlin/TerminalController.kt
@@ -8,33 +8,22 @@ import java.util.concurrent.Executors
 class TerminalController(val name: String, numOfTerminals: Int, val clientsNum: Int) {
     private val executorService: ExecutorService = Executors.newFixedThreadPool(numOfTerminals)
     private var timer: Long = 0
-    private var canShotdown: Boolean = false
-//    init {
-//        repeat(numOfTerminals) {
-////            terminals.add(GlobalScope.async { })
-//            contexts.add(newSingleThreadContext("Terminal #$it"))
-//            terminals.add(CoroutineScope(contexts.last()).async {  })
-//        }
-//    }
 
-    suspend fun serveClient(client: Client, index: Int) {
-// Find a free handler
+    private fun makeOrderOnTerminal(client: Client) {
+        Thread.sleep(0,10 * client.orderTime)
+    }
+
+    fun serveClient(client: Client, index: Int) {
         executorService.submit {
             makeOrderOnTerminal(client)
             if (index == 0) {
                 startTimer()
             } else if (index == clientsNum - 1) {
                 executorService.shutdown()
-                println("ExecutorServiceShutDOwn")
                 val time = stopTimer()
                 println("Время работы кода ${name}: ${(time / 1000.0)} секунд")
             }
         }
-    }
-
-    private fun makeOrderOnTerminal(client: Client) {
-
-        Thread.sleep(0,10 * client.orderTime)
     }
 
     fun startTimer() {
@@ -44,6 +33,4 @@ class TerminalController(val name: String, numOfTerminals: Int, val clientsNum: 
     fun stopTimer(): Long {
         return System.currentTimeMillis() - timer
     }
-
-
 }
